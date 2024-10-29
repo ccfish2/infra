3b1b48d32a9d977afcdc7200540b6b8d9d860d81
ccfish2 <jimin.susan.hu@gmail.com>
2024-10-28 22:34:55 -0700

    module metadata and its print method

--- pkg/hive/cell/info.go
+++ pkg/hive/cell/info.go
@@ -1,11 +1,49 @@
 package cell
 
-import "io"
+import (
+	"fmt"
+	"io"
+	"strings"
+)
+
+const (
+	identBy = 4
+)
 
 type InfoPrinter struct {
 	io.Writer
 	width int
 }
+
 type Info interface {
 	Print(indent int, w *InfoPrinter)
+}
+
+type InfoNode struct {
+	header    string
+	condensed bool
+
+	children []Info
+}
+
+func NewInfoNode(header string) *InfoNode {
+	return &InfoNode{header: header}
+}
+
+func (n *InfoNode) Add(child Info) {
+	n.children = append(n.children, child)
+}
+
+func (n *InfoNode) Print(indent int, w *InfoPrinter) {
+	if n.header != "" {
+		fmt.Fprintf(w, "%s%s:\n", strings.Repeat(" ", indent), n.header)
+		indent += identBy
+	}
+
+	for i, child := range n.children {
+		child.Print(indent, w)
+		if !n.condensed && i != len(n.children)-1 {
+			w.Write([]byte{'\n'})
+		}
+	}
 }
--- pkg/hive/cell/module.go
+++ pkg/hive/cell/module.go
@@ -1,5 +1,40 @@
 package cell
 
-type ModuleID string
+import "regexp"
 
+func Module(id, title string, cells ...Cell) Cell {
+	validateIDAndTitle(id, title)
+	return &module{id, title, cells}
+}
+
+var (
+	idRegex    = regexp.MustCompile(`^[a-z][a-z0-9_\-]{1,30}$`)
+	titleRegex = regexp.MustCompile(`^[a-zA-Z0-9_\- ]{1,80}$`)
+)
+
+func validateIDAndTitle(id, title string) {
+	if !idRegex.MatchString(id) {
+		panic("Module ID format is not correct")
+	}
+	if !titleRegex.MatchString(title) {
+		panic("Title format is not correct")
+	}
+}
+
+type module struct {
+	id    string
+	title string
+	cells []Cell
+}
+
+// Info implements Cell.
+func (m *module) Info(c container) Info {
+	n := NewInfoNode(" " + m.id + " (" + m.title)
+	for _, cell := range m.cells {
+		n.Add(cell.Info(c))
+	}
+	return n
+}
+
+type ModuleID string
 type FullModuleID []string
