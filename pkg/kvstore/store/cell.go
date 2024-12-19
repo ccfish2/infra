eba0d133fc96787768624336846825229af4cd50
ccfish2 <jimin.susan.hu@gmail.com>
2024-12-19 09:40:17 -0800

    proto watcher ifra

--- pkg/kvstore/store/cell.go
+++ pkg/kvstore/store/cell.go
@@ -1,7 +1,40 @@
 package store
 
+import "github.com/ccfish2/infra/pkg/hive/cell"
+
+var Cell = cell.Module(
+	"kvstore-utils",
+	"Manage kvstore controllers",
+	cell.Provide(NewFactory),
+)
+
 type Factory interface {
 	NewSyncStore(clusterName string, backend SyncStoreBackend, prefix string, opts ...WSSOpt) SyncStore
 	NewWatchStore(clusterName string, keyCreator KeyCreator, observer Observer, opts ...RWSOpt) WatchStore
 	NewWatchStoreManager(backend WatchStoreBackend, clusterName string) WatchStoreManager
+}
+
+type factoryImpl struct {
+	metrics *Metrics
+}
+
+// NewSyncStore implements Factory.
+func (f *factoryImpl) NewSyncStore(clusterName string, backend SyncStoreBackend, prefix string, opts ...WSSOpt) SyncStore {
+	panic("unimplemented")
+}
+
+// NewWatchStore implements Factory.
+func (f *factoryImpl) NewWatchStore(clusterName string, keyCreator KeyCreator, observer Observer, opts ...RWSOpt) WatchStore {
+	panic("unimplemented")
+}
+
+// NewWatchStoreManager implements Factory.
+func (f *factoryImpl) NewWatchStoreManager(backend WatchStoreBackend, clusterName string) WatchStoreManager {
+	panic("unimplemented")
+}
+
+func NewFactory(storeMetrics *Metrics) Factory {
+	return &factoryImpl{
+		metrics: storeMetrics,
+	}
 }
---
+++ pkg/kvstore/store/metrics.go
@@ -0,0 +1,11 @@
+package store
+
+import (
+	"github.com/ccfish2/infra/pkg/metrics/metric"
+)
+
+type Metrics struct {
+	KVStoreSyncQueueSize        metric.Vec[metric.Gauge]
+	KVStoreSyncErrors           metric.Vec[metric.Counter]
+	KVStoreInitialSyncCompleted metric.Vec[metric.Gauge]
+}
