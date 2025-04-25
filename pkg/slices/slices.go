8adf0d4b45b5e60cfebc3e70470da07ab6a1d9a2
ccfish2 <jimin.susan.hu@gmail.com>
2025-04-25 16:48:24 -0700

    add deduplicate func Unique

--- pkg/slices/slices.go
+++ pkg/slices/slices.go
@@ -6,6 +6,41 @@ import (
 	"golang.org/x/exp/constraints"
 )
 
+// deduplicates the elements in the input slice, preserving their ordering and
+// modifying the slice in place.
+func Unique[S ~[]T, T comparable](s S) S {
+	if len(s) < 2 {
+		return s
+	}
+
+	last := 0
+
+	if len(s) < 192 {
+	Loop:
+		for i := 0; i < len(s); i++ {
+			for j := 0; j < last; j++ {
+				if s[i] == s[j] {
+					continue Loop
+				}
+			}
+			s[last] = s[i]
+			last++
+		}
+	} else {
+		set := make(map[T]struct{}, len(s))
+		for i := 0; i < len(s); i++ {
+			if _, ok := set[s[i]]; ok {
+				continue
+			}
+			set[s[i]] = struct{}{}
+			s[last] = s[i]
+			last++
+		}
+	}
+
+	return s[:last]
+}
+
 func SortedUniqs[S ~[]T, T constraints.Ordered](s S) S {
 	return slices.Compact(s)
 }
