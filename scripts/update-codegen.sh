62a376180690434ec6ea1774dc553c26e2d29a59
Jimin Hu <hujimin@example.com>
2026-09-07 18:30:29 -0700

    fix deepcopy and deepequal

--- scripts/update-codegen.sh
+++ scripts/update-codegen.sh
@@ -5,14 +5,12 @@ set -o nounset
 set -o pipefail
 
 SCRIPT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)/.."
-GOPATH=${GOPATH:-$(go env GOPATH)}
-CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${GOPATH}/pkg/mod/k8s.io && ls -d code-generator* | head -1)}
+GOPATH=/Users/jiminhu/go
+CODEGEN_PKG=$GOPATH/src/github.com/code-generator
 
-source "${GOPATH}/pkg/mod/${CODEGEN_PKG}/kube_codegen.sh"
+source "${CODEGEN_PKG}/kube_codegen.sh"
 
-TMPDIR=${TMPDIR:-/tmp/codegen}
-mkdir -p "${TMPDIR}"
-
+TMPDIR=${1}
 PLURAL_EXCEPTIONS="DolphinEndpoints:DolphinEndpoints,DolphinEnvoyConfig:DolphinEnvoyConfigs,DolphinEndpointSlice:DolphinEndpointSlices,DolphinIdentity:DolphinIdentities,DolphinNode:DolphinNodes,DolphinGatewayClassConfig:DolphinGatewayClassConfigs"
 
 # Generate deepcopy methods
@@ -20,14 +18,16 @@ kube::codegen::gen_helpers \
     --boilerplate "${SCRIPT_ROOT}/hack/custom-boilerplate.go.txt" \
     "${SCRIPT_ROOT}/pkg/k8s/apis"
 
-# Generate client, informers, listers
 kube::codegen::gen_client \
-    --input-pkg-root github.com/ccfish2/infra/pkg/k8s/apis \
-    --output-pkg-root github.com/ccfish2/infra/pkg/k8s/client \
-    --output-base "${TMPDIR}" \
+    "./pkg/k8s/apis" \
     --with-watch \
+    --output-dir "${TMPDIR}/github.com/ccfish2/infra/pkg/k8s/client" \
+    --output-pkg "github.com/ccfish2/infra/pkg/k8s/client" \
     --plural-exceptions ${PLURAL_EXCEPTIONS} \
-    --boilerplate "${SCRIPT_ROOT}/hack/custom-boilerplate.go.txt" \
-    ./pkg/k8s/apis
+    --boilerplate "${SCRIPT_ROOT}/hack/custom-boilerplate.go.txt"
 
 cp -r "${TMPDIR}/github.com/ccfish2/infra/." ./
+
+# kube::codegen::gen_helpers \
+#     --boilerplate "${SCRIPT_ROOT}/hack/custom-boilerplate.go.txt" \
+#     "$PWD/pkg/k8s/apis"
