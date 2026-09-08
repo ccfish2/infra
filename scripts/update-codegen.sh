96cdf0f1329e5667cdedc180cac78b1509c8a46f
Jimin Hu <hujimin@example.com>
2026-09-07 18:53:27 -0700

    add missed docs.go

--- scripts/update-codegen.sh
+++ scripts/update-codegen.sh
@@ -5,19 +5,22 @@ set -o nounset
 set -o pipefail
 
 SCRIPT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)/.."
-GOPATH=/Users/jiminhu/go
-CODEGEN_PKG=$GOPATH/src/github.com/code-generator
+GOPATH=${GOPATH:-$(go env GOPATH)}
+CODEGEN_PKG="${GOPATH}/pkg/mod/k8s.io/code-generator@v0.35.6"
 
 source "${CODEGEN_PKG}/kube_codegen.sh"
 
-TMPDIR=${1}
+TMPDIR="${TMPDIR:-/tmp/codegen-$(date +%s)}"
+mkdir -p "${TMPDIR}"
+
 PLURAL_EXCEPTIONS="DolphinEndpoints:DolphinEndpoints,DolphinEnvoyConfig:DolphinEnvoyConfigs,DolphinEndpointSlice:DolphinEndpointSlices,DolphinIdentity:DolphinIdentities,DolphinNode:DolphinNodes,DolphinGatewayClassConfig:DolphinGatewayClassConfigs"
 
-# Generate deepcopy methods
+# Generate deepcopy and deepequal methods
 kube::codegen::gen_helpers \
     --boilerplate "${SCRIPT_ROOT}/hack/custom-boilerplate.go.txt" \
     "${SCRIPT_ROOT}/pkg/k8s/apis"
 
+# Generate client, informers, and listers
 kube::codegen::gen_client \
     "./pkg/k8s/apis" \
     --with-watch \
@@ -26,8 +29,12 @@ kube::codegen::gen_client \
     --plural-exceptions ${PLURAL_EXCEPTIONS} \
     --boilerplate "${SCRIPT_ROOT}/hack/custom-boilerplate.go.txt"
 
-cp -r "${TMPDIR}/github.com/ccfish2/infra/." ./
+# Remove old generated client files (use sudo if needed)
+sudo rm -rf "${SCRIPT_ROOT}/pkg/k8s/client/clientset" \
+            "${SCRIPT_ROOT}/pkg/k8s/client/informers" \
+            "${SCRIPT_ROOT}/pkg/k8s/client/listers"
 
-# kube::codegen::gen_helpers \
-#     --boilerplate "${SCRIPT_ROOT}/hack/custom-boilerplate.go.txt" \
-#     "$PWD/pkg/k8s/apis"
+# Copy generated files back to project
+cp -r "${TMPDIR}/github.com/ccfish2/infra/pkg/k8s/client/." "${SCRIPT_ROOT}/pkg/k8s/client/"
+
+echo "Code generation complete!"
